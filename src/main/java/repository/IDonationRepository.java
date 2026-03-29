3a1f87f0c03ee2a3e5b8502b1b09a9b0c2f08ffb
RelebohileMolefe2 <164934121+RelebohileMolefe2@users.noreply.github.com>
2026-03-29 17:19:35 +0200

    updated rep

--- src/main/java/repository/IDonationRepository.java
+++ src/main/java/repository/IDonationRepository.java
@@ -1,8 +1,8 @@
 package repository;
 
-import domain.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+import domain.Donation;
 public interface IDonationRepository extends JpaRepository<Donation, Integer> {
     // Spring Data JPA will auto-generate CRUD methods
 }
